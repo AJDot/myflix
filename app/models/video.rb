5c8688a6644f710076ce223e2ec8b8351355c6e2
Alex Henegar <alex.henegar19@gmail.com>
2018-03-01 19:18:35 -0500

    setup elasticsearch for video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,13 @@
 class Video < ActiveRecord::Base
+  include Elasticsearch::Model
+  # add active record callbacks on video model to elasticsearch is always synced
+  include Elasticsearch::Model::Callbacks
+  # keeps dev and test environment separate - just like we do for databases
+  index_name ["myflix", Rails.env].join('_')
+  unless self.__elasticsearch__.index_exists?
+    self.__elasticsearch__.create_index!
+  end
+
   belongs_to :category
   has_many :reviews, ->{ order("created_at DESC") }
 
