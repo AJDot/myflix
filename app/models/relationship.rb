fb3e53fc4d6976da0c9bbef51c00946c2192b819
Alex Henegar <alex.henegar19@gmail.com>
2018-03-04 15:11:26 -0500

    make all child records of user delete when user is deleted

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,4 @@
 class Relationship < ActiveRecord::Base
   belongs_to :leader, class_name: 'User'
   belongs_to :follower, class_name: 'User'
-
-  # validates_uniqueness_of :follower, scope: :leader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
 
   has_secure_password validations: false
 
-  has_many :queue_items, -> { order("position") }
-  has_many :reviews, -> { order("created_at DESC") }
-  has_many :following_relationships, class_name: 'Relationship', foreign_key: :follower_id
-  has_many :leading_relationships, class_name: 'Relationship', foreign_key: :leader_id
+  has_many :queue_items, -> { order("position") }, dependent: :destroy
+  has_many :reviews, -> { order("created_at DESC") }, dependent: :destroy
+  has_many :following_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  has_many :leading_relationships, class_name: 'Relationship', foreign_key: :leader_id, dependent: :destroy
 
   def normalize_queue_item_positions
     queue_items.each_with_index do |queue_item, index|
