58295900685ebd71c9e330072a1b43807d81e4ec
Alex Henegar <alex.henegar19@gmail.com>
2018-03-04 19:05:46 -0500

    fix video sorting again, last implemenation broke tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,7 +16,11 @@ def sort_videos
       b.title <=> a.title
     end
   when 'rating'
-    videos = videos.sort_by(&:rating)
+    videos = videos.sort do |a, b|
+      a_r = a.rating || 0
+      b_r = b.rating || 0
+      b_r <=> a_r
+    end
   else # 'a-z'
     videos = videos.sort_by(&:title)
   end
